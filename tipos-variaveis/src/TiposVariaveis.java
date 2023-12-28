5ca4dd5c8af8d8ec3550fb3a9cbd85c9df13e48a
luis2922345 <luis@connectinfo.com.br>
2023-12-28 15:11:10 -0300

    mais atualizações

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,8 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.44;
+        
+    final double VALOR_DE_PI = 3.14;
+        System.err.println(VALOR_DE_PI);
+    }
+}
